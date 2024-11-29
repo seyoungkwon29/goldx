0aef7619a180a5a4dff3455847541fe124eb8775
seyoungkwon <seyoungkwon29@gmail.com>
2024-11-29 21:52:59 +0900

    first commit

---
+++ src/main/java/org/zerob/goldx/GoldxApplication.java
@@ -0,0 +1,13 @@
+package org.zerob.goldx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoldxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerob/goldx/GoldxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerob.goldx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
