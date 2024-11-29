a4dd4b2c3a1129d89893b3b6de928c743f3675bb
seyoungkwon <seyoungkwon29@gmail.com>
2024-11-30 01:32:50 +0900

    feat: PostgreSQL Connection Test

---
+++ src/test/java/org/zerob/goldx/PostgreSQLConnectionTest.java
@@ -0,0 +1,32 @@
+package org.zerob.goldx;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class PostgreSQLConnectionTest {
+
+    @Value("${spring.datasource.url}")
+    private String URL;
+    @Value("${spring.datasource.username}")
+    private String USERNAME;
+    @Value("${spring.datasource.password}")
+    private String PASSWORD;
+
+    @Test
+    @DisplayName("PostgreSQL 연결 테스트")
+    public void ConnectionTest() throws Exception {
+        Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD); // DB 연결
+        Assertions.assertNotNull(connection);
+    }
+
+
+}
