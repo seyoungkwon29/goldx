565c89b357bec7f7b36f30bfdabcee6d44be1663
seyoungkwon <seyoungkwon29@gmail.com>
2024-12-02 12:50:15 +0900

    데이터베이스 조회 테스트

--- src/test/java/org/zerob/goldx/PostgreSQLConnectionTest.java
+++ src/test/java/org/zerob/goldx/PostgreSQLConnectionTest.java
@@ -1,5 +1,6 @@
 package org.zerob.goldx;
 
+import org.assertj.core.api.Fail;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,8 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -28,4 +28,22 @@ public class PostgreSQLConnectionTest {
         Assertions.assertNotNull(connection);
     }
 
+    @Test
+    @DisplayName("DB 연결 및 특정 ID의 이름 검증")
+    public void FindByIdTest() throws Exception {
+        // Given
+        try {
+            Connection con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            Statement pre = con.createStatement();
+            ResultSet rs = pre.executeQuery("select * from member where id = 1");
+
+            String username = null;
+            if (rs.next()) {
+                username = rs.getString("username");
+            }
+            Assertions.assertEquals("Tim", username);
+        } catch (SQLException e) {
+            Fail.fail("테스트에 예외가 발생했습니다." + e.getMessage());
+        }
+    }
 }
